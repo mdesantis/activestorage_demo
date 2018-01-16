93f7ef8f686ce9c2b67b563b613810f11ad13adc
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-01-16 17:26:36 +0100

    Fix ActiveStorage jobs conflict with SQLite

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,10 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # Necessary for an issue with ActiveJob and SQLite:
+  # https://github.com/rails/rails/issues/30937
+  config.active_job.queue_adapter = :inline
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
