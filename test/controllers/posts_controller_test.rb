b109f03e5ae6d53346051c39ecf623e640e22203
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-01-16 10:28:11 +0100

    Fix Posts controller tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    set_admin_auth_headers
     @post = posts(:one)
   end
 
@@ -11,13 +12,13 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
-    get new_post_url
+    get new_post_url, headers: @auth_headers
     assert_response :success
   end
 
   test 'should create post' do
     assert_difference('Post.count') do
-      post posts_url, params: { post: { body: @post.body, title: @post.title } }
+      post posts_url, params: { post: { body: @post.body, title: @post.title } }, headers: @auth_headers
     end
 
     assert_redirected_to post_url(Post.last)
@@ -29,18 +30,18 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get edit' do
-    get edit_post_url(@post)
+    get edit_post_url(@post), headers: @auth_headers
     assert_response :success
   end
 
   test 'should update post' do
-    patch post_url(@post), params: { post: { body: @post.body, title: @post.title } }
+    patch post_url(@post), params: { post: { body: @post.body, title: @post.title } }, headers: @auth_headers
     assert_redirected_to post_url(@post)
   end
 
   test 'should destroy post' do
     assert_difference('Post.count', -1) do
-      delete post_url(@post)
+      delete post_url(@post), headers: @auth_headers
     end
 
     assert_redirected_to posts_url
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,12 @@ class ActiveSupport::TestCase # rubocop:disable Style/ClassAndModuleChildren
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def set_admin_auth_headers
+    username = ApplicationController.admin_username
+    password = ApplicationController.admin_password
+    encoded_credentials = Base64.encode64("#{username}:#{password}")
+
+    @auth_headers = { 'Authorization' => "Basic #{encoded_credentials}" }
+  end
 end
