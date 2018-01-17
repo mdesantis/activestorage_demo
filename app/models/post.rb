2bfadd2e2216d41b64fb2ed61e2c286bf8a70833
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-01-17 16:35:59 +0100

    Add unsaved attachments cache

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many_attached :gallery_images
 
   validates_presence_of :title
+
+  def unsaved_gallery_images_attachments
+    gallery_images_attachments.select { |attachment| attachment.new_record? && attachment.blob.present? }
+  end
 end
